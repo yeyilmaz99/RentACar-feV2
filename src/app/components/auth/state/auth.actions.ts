32329ff5d5b79296a1a07cbc87e692281759c806
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-10 17:13:24 +0300

    login&signin&logout ngrx

---
+++ src/app/components/auth/state/auth.actions.ts
@@ -0,0 +1,33 @@
+import { createAction, props } from "@ngrx/store"
+import { Register } from "src/app/models/register.model";
+import { User } from "src/app/models/user.model";
+
+
+export const LOGIN_START = '[auth page] login start'
+export const LOGIN_SUCCESS = '[auth page] login success'
+export const LOGIN_FAIL = '[auth page] login fail'
+
+
+export const SIGNUP_START = '[auth page] signup start';
+export const SIGNUP_SUCCESS = '[auth page] signup success';
+
+export const AUTO_LOGIN_ACTION = '[auth page] auto login';
+
+export const LOGOUT_ACTION ='[auth page] logout'
+
+
+export const loginStart = createAction(
+    LOGIN_START,
+    props<{ email: string; password: string }>()
+  );
+
+
+export const loginSuccess = createAction(LOGIN_SUCCESS, props<{user:User , redirect:boolean, admin:boolean}>())
+
+export const loginFail = createAction(LOGIN_FAIL)
+
+export const signupStart = createAction(SIGNUP_START, props<{register:Register}>())
+export const signupSuccess = createAction(SIGNUP_SUCCESS, props<{user:User, redirect:boolean,admin:boolean}>())
+
+export const autoLogin = createAction(AUTO_LOGIN_ACTION);
+export const autoLogout = createAction(LOGOUT_ACTION);
