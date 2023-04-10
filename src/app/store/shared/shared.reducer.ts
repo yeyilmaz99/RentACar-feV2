b52adda0c788baef0ea9ad3a34e8d85373e7792c
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-10 17:14:35 +0300

    login&signin&logout ngrx

---
+++ src/app/store/shared/shared.reducer.ts
@@ -0,0 +1,23 @@
+import { createReducer, on } from "@ngrx/store";
+import { setErrorMessage, setLoadingSpinner } from "./shared.actions";
+import { initialState } from "./shared.state";
+
+
+
+const _sharedReducer = createReducer(initialState, on(setLoadingSpinner,(state,action) => {
+    return {
+        ...state,
+        showLoading: action.status,
+    };
+}), on(setErrorMessage, (state,action) => {
+    return {
+        ...state,
+        errorMessage: action.message
+    }
+})
+)
+
+
+export function SharedReducer (state:any, action:any){
+    return _sharedReducer(state,action);
+}
