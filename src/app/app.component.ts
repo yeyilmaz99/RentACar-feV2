9b8e31ed72c7a948c59c3e08d852f7c5eb982464
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-10 17:12:58 +0300

    login&signin&logout ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppState } from './store/app.state';
+import { Store } from '@ngrx/store';
+import { autoLogin } from './components/auth/state/auth.actions';
+import { getErrorMessage, getLoading } from './store/shared/shared.selector';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'recapProject-feV2';
+  showLoading:Observable<boolean>;
+  errorMessage:Observable<string>;
+
+  constructor(private store:Store<AppState>){
+
+  }
+
+  ngOnInit(): void {
+    this.showLoading = this.store.select(getLoading);
+    this.errorMessage = this.store.select(getErrorMessage);
+    if(localStorage.getItem('userData')){
+      this.store.dispatch(autoLogin());
+    }
+
+    
+  }
 }
