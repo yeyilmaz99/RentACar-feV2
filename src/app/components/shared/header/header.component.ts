11c54fe01aed97b1918bbe60befcce8f80c0287e
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-10 17:14:04 +0300

    login&signin&logout ngrx

---
+++ src/app/components/shared/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store/app.state';
+import { autoLogout } from '../../auth/state/auth.actions';
+import { isAuthenticated } from '../../auth/state/auth.selector';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  isAuthenticated:Observable<boolean>;
+  constructor(private store:Store<AppState>) { }
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.store.select(isAuthenticated);
+  }
+
+
+  onLogout(event:Event){
+    event.preventDefault();
+    this.store.dispatch(autoLogout())
+  }
+
+}
