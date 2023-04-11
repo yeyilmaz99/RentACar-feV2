28d78fb57bdd31469a6d6f85d3ce5460aca8eb3b
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-11 17:12:00 +0300

    car page

--- src/app/components/car/cars/cars.component.ts
+++ src/app/components/car/cars/cars.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Car } from 'src/app/models/car.model';
+import { getCars } from '../state/car.selector';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.state';
+import { loadCars } from '../state/car.actions';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { OrderByPipe } from 'src/app/pipes/order-by.pipe';
 
 @Component({
   selector: 'app-cars',
@@ -6,10 +14,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cars.component.css']
 })
 export class CarsComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  cars:Car[];
+  carsSlice: Car[];
+  filterText: string = '';
+  constructor(private store:Store<AppState>,private orderByPipe:OrderByPipe,private cdRef:ChangeDetectorRef ) { }
 
   ngOnInit(): void {
+    this.getCars()
   }
+
+  getCars(){
+    this.store.dispatch(loadCars());
+    this.store.select(getCars).subscribe(response => {
+      this.cars = response
+      this.carsSlice = response.slice(0,12)
+    })
+  }
+
+  paginatorLength(){
+    if(this.cars){
+      return this.cars.length
+    }
+    return undefined
+  }
+
+  sortAll(){
+    this.cars = this.orderByPipe.transform(this.cars, 'carId', 'asc')
+    this.carsSlice = this.cars.slice(0,12)
+    this.paginator.firstPage();
+  }
+  sortLow(){
+    this.cars = this.orderByPipe.transform(this.cars, 'dailyPrice', 'asc')
+    this.carsSlice = this.cars.slice(0,12)
+    this.paginator.firstPage();
+  }
+  sortHigh(){
+    this.cars = this.orderByPipe.transform(this.cars, 'dailyPrice', 'desc')
+    this.carsSlice = this.cars.slice(0,12)
+    this.paginator.firstPage();
+  }
+
+
+  OnPageChange(event: PageEvent) {
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if (endIndex > this.cars.length) {
+      endIndex = this.cars.length;
+    }
+    this.carsSlice = this.cars.slice(startIndex, endIndex);
+  }
+
+
+
+
+
+
 
 }
