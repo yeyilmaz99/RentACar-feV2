cfc873064875c31f892542014f11527bb89d42db
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-11 17:12:10 +0300

    car page

---
+++ src/app/models/carDetails.model.ts
@@ -0,0 +1,9 @@
+export interface CarDetails{
+    carId:number,
+    carName:string,
+    brandName:string,
+    colorName:string,
+    dailyPrice:number,
+    description:string,
+    findeksPoint:string
+}
