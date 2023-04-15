9fe8c84e7ccfe0cf156435d11dd758bc0c105e7c
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-15 18:49:25 +0300

    carDetail & colorservice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CarDetailsComponent } from './components/car/car-details/car-details.component';
 
 const routes: Routes = [
   {
@@ -9,8 +10,10 @@ const routes: Routes = [
   {
     path: 'auth', loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
   },
-  { path: 'car', loadChildren: () => import('./components/car/car.module').then(m => m.CarModule) },
-  { path: 'brands', loadChildren:() => import('./components/brand/brand.module').then(m => m.BrandModule)}
+  { path: 'cars', loadChildren: () => import('./components/car/car.module').then(m => m.CarModule) },
+  { path: 'brands', loadChildren:() => import('./components/brand/brand.module').then(m => m.BrandModule)},
+  { path: 'cars/car/:id', component: CarDetailsComponent }
+
 ];
 
 @NgModule({
