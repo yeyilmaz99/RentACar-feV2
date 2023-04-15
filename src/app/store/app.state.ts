522e468da927e8786d33290e1958ec7404f66543
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-15 18:49:51 +0300

    carDetail & colorservice

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -6,16 +6,21 @@ import { SharedState } from "./shared/shared.state";
 import { AuthState } from "../components/auth/state/auth.state";
 import { AuthReducer } from "../components/auth/state/auth.reducer";
 import { AUTH_STATE_NAME } from "../components/auth/state/auth.selector";
+import { COLOR_STATE_NAME } from "../components/color/state/color.selector";
+import { ColorState } from "../components/color/state/color.state";
+import { colorReducer } from "../components/color/state/color.reducer";
 
 
 export interface AppState {
     [AUTH_STATE_NAME]:AuthState
     [SHARED_STATE_NAME]:SharedState;
+    [COLOR_STATE_NAME]:ColorState
     router:RouterReducerState;
 }
 
 export const appReducer = {
     [SHARED_STATE_NAME]:SharedReducer,
     [AUTH_STATE_NAME]:AuthReducer,
+    [COLOR_STATE_NAME]:colorReducer,
     router:routerReducer
 }
