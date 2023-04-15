bbaa4bdf0ad8ec57659ca901cb858510bc194e93
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-15 18:49:33 +0300

    carDetail & colorservice

--- src/app/components/car/car-details/car-details.component.ts
+++ src/app/components/car/car-details/car-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Car } from 'src/app/models/car.model';
+import { AppState } from 'src/app/store/app.state';
+import { getCarById } from '../state/car.selector';
+import { setLoadingSpinner } from 'src/app/store/shared/shared.actions';
 
 @Component({
   selector: 'app-car-details',
@@ -6,10 +12,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-details.component.css']
 })
 export class CarDetailsComponent implements OnInit {
-
-  constructor() { }
+  car:Observable<Car>
+  constructor(private store:Store<AppState>) { }
 
   ngOnInit(): void {
+    this.car = this.store.select(getCarById);
+    this.store.dispatch(setLoadingSpinner({status:false}))
   }
-
 }
+
