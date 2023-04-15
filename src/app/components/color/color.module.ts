8e484ded0d99c01d6ecb0ba0c9fbce73075ba308
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-15 18:49:37 +0300

    carDetail & colorservice

---
+++ src/app/components/color/color.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { EffectsModule } from "@ngrx/effects";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ColorEffects } from "./state/color.effects";
+import { COLOR_STATE_NAME } from "./state/color.selector";
+import { colorReducer } from "./state/color.reducer";
+import { StoreModule } from "@ngrx/store";
+
+
+
+
+const routes: Routes = [
+
+    {path: '', children: [
+    ]}
+
+
+]
+
+
+
+@NgModule({
+    declarations: [
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MatPaginatorModule,
+        RouterModule.forChild(routes),
+        EffectsModule.forFeature([ColorEffects]),
+        FormsModule,
+        StoreModule.forFeature(COLOR_STATE_NAME,colorReducer)
+    ],
+
+    providers:[]
+})
+
+
+export class ColorModule {
+
+}
