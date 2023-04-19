150b301365b6594abb0d120bb5a7863dfdafcaa1
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-19 23:44:15 +0300

    getCarDetails

--- src/app/services/car/car.service.ts
+++ src/app/services/car/car.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Car } from 'src/app/models/car.model';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
+import { SingleResponseModel } from 'src/app/models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,11 @@ export class CarService {
   getCars(): Observable<ListResponseModel<Car>> {
     let newPath = this.apiUrl + 'Cars/getcarsdetails';
     return this.httpClient.get<ListResponseModel<Car>>(newPath);
+  }
+
+  getCarById(id:number):Observable<SingleResponseModel<Car>>{
+    let newPath = this.apiUrl + "Cars/getcardetails?carId=";
+    console.log("carservice triggered")
+    return this.httpClient.get<SingleResponseModel<Car>>(newPath+id);
   }
 }
