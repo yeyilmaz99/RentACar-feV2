8a34506ef4ff9cfd077277196ac78643f74a5701
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-21 02:07:44 +0300

    brand crud operations

--- src/app/components/brand/state/brand.effects.ts
+++ src/app/components/brand/state/brand.effects.ts
@@ -4,8 +4,8 @@ import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
 import { AppState } from "src/app/store/app.state";
 import { setLoadingSpinner } from "src/app/store/shared/shared.actions";
-import { addBrand, addBrandSuccess, loadBrands, loadBrandsSuccess } from "./brand.actions";
-import { map, mergeMap } from "rxjs";
+import { addBrand, addBrandSuccess, deleteBrand, deleteBrandSuccess, loadBrands, loadBrandsSuccess } from "./brand.actions";
+import { map, mergeMap, switchMap } from "rxjs";
 import { BrandService } from "src/app/services/brand/brand.service";
 import { ToastrService } from "ngx-toastr";
 
@@ -17,7 +17,6 @@ export class BrandEffects{
 
     loadBrands$ = createEffect(() => {
         return this.actions$.pipe(ofType(loadBrands), mergeMap((action) => {
-
             return this.brandService.getBrands().pipe(map((response) => {
                 // this.store.dispatch(setLoadingSpinner({status:false}))
                 const brands = response.data
@@ -27,7 +26,7 @@ export class BrandEffects{
     })
 
 
-    addPost$ = createEffect(() => {
+    addBrand$ = createEffect(() => {
       return this.actions$.pipe(ofType(addBrand), mergeMap(action => {
           return this.brandService.addBrand(action.brand).pipe(map(response => {
               const message = response.message
@@ -35,6 +34,18 @@ export class BrandEffects{
               return addBrandSuccess({ message });
           }))
       }))
-  },)
+    },)
+
+    deleteBrand$ = createEffect(() => {
+        return this.actions$.pipe(ofType(deleteBrand), switchMap((action) => {
+            return this.brandService.deleteBrand(action.brand).pipe(map((data) => {
+                return deleteBrandSuccess({ id:action.brand.brandId })
+            }))
+        }))
+    })
+
+
+
+
 
 }
