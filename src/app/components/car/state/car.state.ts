331e6cbd9a65c2e3d6961322a1654f3c7fe9b093
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-20 23:38:43 +0300

    carImages

--- src/app/components/car/state/car.state.ts
+++ src/app/components/car/state/car.state.ts
@@ -1,11 +1,14 @@
 import { Car } from "src/app/models/car.model";
+import { CarImage } from "src/app/models/carImage";
 
 
 
 export interface CarState {
     cars:Car[];
+    carImages:CarImage[];
 }
 
 export const initialState:CarState = {
     cars:null,
+    carImages:null
 }
