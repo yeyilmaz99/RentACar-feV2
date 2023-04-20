d60985ed1043ae4ebaaeb2c843e06063c7476252
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-20 23:38:40 +0300

    carImages

--- src/app/components/car/state/car.effects.ts
+++ src/app/components/car/state/car.effects.ts
@@ -4,7 +4,7 @@ import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
 import { filter, map, mergeMap, switchMap, tap } from "rxjs";
 import { AppState } from "src/app/store/app.state";
-import { loadCars, loadCarsSuccess } from "./car.actions";
+import { loadCarImages, loadCarImagesSuccess, loadCars, loadCarsSuccess } from "./car.actions";
 import { CarService } from "src/app/services/car/car.service";
 import { setLoadingSpinner } from "src/app/store/shared/shared.actions";
 import { ROUTER_NAVIGATION, RouterNavigatedAction } from "@ngrx/router-store";
@@ -13,7 +13,6 @@ import { Car } from "src/app/models/car.model";
 @Injectable()
 export class CarEffects {
     constructor(private actions$: Actions, private carService: CarService, private store: Store<AppState>, private router: Router) {
-
     }
 
     loadCar$ = createEffect(() => {
@@ -26,6 +25,8 @@ export class CarEffects {
             }))
         }))
     })
+
+    
 
 
 
@@ -46,5 +47,22 @@ export class CarEffects {
         })
         )
     })
+
+    getCarImages$ = createEffect(()=> {
+        return this.actions$.pipe(ofType(ROUTER_NAVIGATION), filter((r: RouterNavigatedAction) => {
+            return r.payload.routerState.url.startsWith('/cars/car')
+        }), map((r: RouterNavigatedAction) => {
+            return r.payload.routerState['params']['id'];
+        }), switchMap((id) => {
+            this.store.dispatch(setLoadingSpinner({status:true}))
+            return this.carService.getCarImagesByCarId(id).pipe(map((response) => {
+                this.store.dispatch(setLoadingSpinner({status:false}))
+                const carImages = response.data
+                console.log(carImages)
+                return loadCarImagesSuccess({ carImages })
+            }))
+        })
+        )
+    },)
 
 }
