a8fa8c07ea878924d48e50759e1ffa354a08772d
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-20 23:38:38 +0300

    carImages

--- src/app/components/car/state/car.actions.ts
+++ src/app/components/car/state/car.actions.ts
@@ -1,16 +1,21 @@
 import { createAction, props } from "@ngrx/store";
 import { Car } from "src/app/models/car.model";
+import { CarImage } from "src/app/models/carImage";
 
 
 
 
 
 export const LOAD_CARS = '[car page] load cars'
+export const LOAD_CAR_IMAGES= '[car page] load images';
 
-
+export const LOAD_CAR_IMAGES_SUCCESS= '[car page] load images success'
 export const LOAD_CARS_SUCCESS = '[car page] load cars success'
 
 
 
 export const loadCars = createAction(LOAD_CARS);
 export const loadCarsSuccess = createAction(LOAD_CARS_SUCCESS, props<{cars:Car[]}>());
+
+export const loadCarImages = createAction(LOAD_CAR_IMAGES);
+export const loadCarImagesSuccess = createAction(LOAD_CAR_IMAGES_SUCCESS, props<{carImages:CarImage[]}>())
