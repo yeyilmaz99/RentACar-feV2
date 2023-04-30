a62c9b57db0f0b06cd98443df5056d7830f43cdd
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-30 22:59:17 +0300

    carAdd

--- src/app/components/car/state/car.effects.ts
+++ src/app/components/car/state/car.effects.ts
@@ -2,21 +2,22 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
-import { filter, map, mergeMap, switchMap, tap } from "rxjs";
+import { catchError, filter, map, mergeMap, of, switchMap, tap } from "rxjs";
 import { AppState } from "src/app/store/app.state";
-import { loadCarImages, loadCarImagesSuccess, loadCars, loadCarsSuccess } from "./car.actions";
+import { addCar, addCarSuccess, loadCarImages, loadCarImagesSuccess, loadCars, loadCarsSuccess } from "./car.actions";
 import { CarService } from "src/app/services/car/car.service";
-import { setLoadingSpinner } from "src/app/store/shared/shared.actions";
+import { setErrorMessage, setLoadingSpinner } from "src/app/store/shared/shared.actions";
 import { ROUTER_NAVIGATION, RouterNavigatedAction } from "@ngrx/router-store";
 import { Car } from "src/app/models/car.model";
+import { ToastrService } from "ngx-toastr";
 
 @Injectable()
 export class CarEffects {
-    constructor(private actions$: Actions, private carService: CarService, private store: Store<AppState>, private router: Router) {
+    constructor(private actions$: Actions, private carService: CarService, private store: Store<AppState>, private router: Router, private toastrService:ToastrService) {
     }
 
     loadCar$ = createEffect(() => {
-        this.store.dispatch(setLoadingSpinner({status:true}))
+        // this.store.dispatch(setLoadingSpinner({status:true}))
         return this.actions$.pipe(ofType(loadCars), mergeMap((action) => {
             return this.carService.getCars().pipe(map((response) => {
                 this.store.dispatch(setLoadingSpinner({status:false}))
@@ -36,7 +37,7 @@ export class CarEffects {
         }), map((r: RouterNavigatedAction) => {
             return r.payload.routerState['params']['id'];
         }), switchMap((id) => {
-            this.store.dispatch(setLoadingSpinner({status:true}))
+            // this.store.dispatch(setLoadingSpinner({status:true}))
             return this.carService.getCarById(id).pipe(map((response) => {
                 const cars: Car[] = [];
                 this.store.dispatch(setLoadingSpinner({status:false}))
@@ -54,7 +55,7 @@ export class CarEffects {
         }), map((r: RouterNavigatedAction) => {
             return r.payload.routerState['params']['id'];
         }), switchMap((id) => {
-            this.store.dispatch(setLoadingSpinner({status:true}))
+            // this.store.dispatch(setLoadingSpinner({status:true}))
             return this.carService.getCarImagesByCarId(id).pipe(map((response) => {
                 this.store.dispatch(setLoadingSpinner({status:false}))
                 const carImages = response.data
@@ -62,6 +63,22 @@ export class CarEffects {
             }))
         })
         )
+    },)
+
+
+    addCar$ = createEffect(() => {
+        return this.actions$.pipe(ofType(addCar), mergeMap(action => {
+            return this.carService.addCar(action.car).pipe(map(response => {
+                const message = response.message
+                this.toastrService.success(response.message);
+                return addCarSuccess({message});
+            }),catchError(errResp => {
+                const errorMessage = errResp.error;
+                this.toastrService.error(errResp.error.message,errResp.error.message)
+                this.store.dispatch(setLoadingSpinner({ status: false }));
+                return of(setErrorMessage({ message: errorMessage }));
+            }))
+        }))
     },)
 
 }
