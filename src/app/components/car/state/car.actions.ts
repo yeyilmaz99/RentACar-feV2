c5799f3cbaff1d1760ec2b8e6c5d20bb0348228b
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-30 22:59:16 +0300

    carAdd

--- src/app/components/car/state/car.actions.ts
+++ src/app/components/car/state/car.actions.ts
@@ -12,6 +12,8 @@ export const LOAD_CAR_IMAGES= '[car page] load images';
 export const LOAD_CAR_IMAGES_SUCCESS= '[car page] load images success'
 export const LOAD_CARS_SUCCESS = '[car page] load cars success'
 
+export const ADD_CAR = '[car page] add car';
+export const ADD_CAR_SUCCESS = '[car page] add car success';
 
 
 export const loadCars = createAction(LOAD_CARS);
@@ -19,3 +21,6 @@ export const loadCarsSuccess = createAction(LOAD_CARS_SUCCESS, props<{cars:Car[]
 
 export const loadCarImages = createAction(LOAD_CAR_IMAGES);
 export const loadCarImagesSuccess = createAction(LOAD_CAR_IMAGES_SUCCESS, props<{carImages:CarImage[]}>())
+
+export const addCar = createAction(ADD_CAR, props<{car:any}>());
+export const addCarSuccess= createAction(ADD_CAR_SUCCESS, props<{message:string}>())
