8268b74069f8f5d3b12928518c9f769567555657
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-05-02 02:55:57 +0300

    deleteCar

--- src/app/components/car/state/car.actions.ts
+++ src/app/components/car/state/car.actions.ts
@@ -1,5 +1,6 @@
 import { createAction, props } from "@ngrx/store";
 import { Car } from "src/app/models/car.model";
+import { CarDelete } from "src/app/models/carDelete";
 import { CarImage } from "src/app/models/carImage";
 
 
@@ -15,6 +16,12 @@ export const LOAD_CARS_SUCCESS = '[car page] load cars success'
 export const ADD_CAR = '[car page] add car';
 export const ADD_CAR_SUCCESS = '[car page] add car success';
 
+export const DELETE_CAR_ACTION = '[car page] delete car action';
+export const DELETE_CAR_SUCCESS = '[car page] delete car success';
+
+export const UPDATE_CAR_ACTION ='[car page] update car action';
+export const UPDATE_CAR_SUCCESS = '[car page] update car success';
+
 
 export const loadCars = createAction(LOAD_CARS);
 export const loadCarsSuccess = createAction(LOAD_CARS_SUCCESS, props<{cars:Car[]}>());
@@ -23,4 +30,11 @@ export const loadCarImages = createAction(LOAD_CAR_IMAGES);
 export const loadCarImagesSuccess = createAction(LOAD_CAR_IMAGES_SUCCESS, props<{carImages:CarImage[]}>())
 
 export const addCar = createAction(ADD_CAR, props<{car:any}>());
-export const addCarSuccess= createAction(ADD_CAR_SUCCESS, props<{message:string}>())
+export const addCarSuccess= createAction(ADD_CAR_SUCCESS, props<{message:string}>());
+
+export const deleteCarAction = createAction(DELETE_CAR_ACTION, props<{carToDelete:CarDelete}>());
+export const deleteCarSuccess = createAction(DELETE_CAR_SUCCESS, props<{message:string}>());
+
+export const updateCarAction = createAction(UPDATE_CAR_ACTION, props<{car:Car}>());
+export const updateCarSuccess = createAction(UPDATE_CAR_SUCCESS, props<{car:Car}>());
+
