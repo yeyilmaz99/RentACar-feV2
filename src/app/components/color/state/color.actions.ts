cc667284a86c8b415feb2881377ade0bf498839f
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-15 03:38:49 +0300

    color-add

--- src/app/components/color/state/color.actions.ts
+++ src/app/components/color/state/color.actions.ts
@@ -9,8 +9,14 @@ export const LOAD_COLORS_SUCCESS = '[color] load colors success';
 export const ADD_COLOR_ACTION = '[color] add color';
 export const ADD_COLOR_SUCCESS = '[color] add color success';
 
+export const DELETE_COLOR_ACTION ='[color] delete color';
+export const DELETE_COLOR_SUCCESS ='[color] delete color success';
+
 export const loadColors = createAction(LOAD_COLORS);
 export const loadColorsSuccess = createAction(LOAD_COLORS_SUCCESS, props<{colors: Color[]}>())
 
 export const addColor = createAction(ADD_COLOR_ACTION, props<{color:Color}>());
 export const addColorSuccess = createAction(ADD_COLOR_SUCCESS, props<{message:string}>())
+
+export const deleteColor = createAction(DELETE_COLOR_ACTION, props<{color:Color}>());
+export const deleteColorSuccess = createAction(DELETE_COLOR_SUCCESS, props<{id:number, message:string}>())
