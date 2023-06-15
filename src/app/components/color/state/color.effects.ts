0e3a6d2a95aefe06de94b6ea0d4af55868742e7c
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-15 20:59:00 +0300

    crud operations

--- src/app/components/color/state/color.effects.ts
+++ src/app/components/color/state/color.effects.ts
@@ -5,63 +5,64 @@ import { Store } from "@ngrx/store"
 import { ToastrService } from "ngx-toastr"
 import { ColorService } from "src/app/services/color.service"
 import { AppState } from "src/app/store/app.state"
-import { addColor, addColorSuccess, deleteColor, deleteColorSuccess, loadColors, loadColorsSuccess } from "./color.actions"
-import { mergeMap,map, switchMap, concatMap, of } from "rxjs"
+import { addColor, addColorSuccess, deleteColor, deleteColorSuccess, loadColors, loadColorsSuccess, updateColor, updateColorSuccess } from "./color.actions"
+import { mergeMap, map, switchMap, concatMap, of } from "rxjs"
 import { merge } from "lodash"
 
 
 @Injectable()
-export class ColorEffects{
-    constructor(private actions$:Actions, private colorService:ColorService, private store:Store<AppState>, private router:Router, private toastrService:ToastrService){}
+export class ColorEffects {
+  constructor(private actions$: Actions, private colorService: ColorService, private store: Store<AppState>, private router: Router, private toastrService: ToastrService) { }
 
-    loadColors$ = createEffect(() => {
-        return this.actions$.pipe(ofType(loadColors), mergeMap((action) => {
+  loadColors$ = createEffect(() => {
+    return this.actions$.pipe(ofType(loadColors), mergeMap((action) => {
 
-            return this.colorService.getColors().pipe(map((response) => {
-                // this.store.dispatch(setLoadingSpinner({status:false}))
-                const colors = response.data
-                return loadColorsSuccess({ colors })
-            }))
-        }))
-    })
+      return this.colorService.getColors().pipe(map((response) => {
+        // this.store.dispatch(setLoadingSpinner({status:false}))
+        const colors = response.data
+        return loadColorsSuccess({ colors })
+      }))
+    }))
+  })
 
-    // addColor$ = createEffect(() => {
-    //     return this.actions$.pipe(ofType(addColor), mergeMap(action => {
-    //         return this.colorService.addColor(action.color).pipe(map(response => {
-    //             const message = response.message
-    //             const color = action.color
-    //             this.toastrService.success(message)
-    //             return addColorSuccess({ message, color });
-    //         }))
-    //     }))
-    //   },)
-    addColor$ = createEffect(() => {
-        return this.actions$.pipe(
-          ofType(addColor),
-          mergeMap((action) => {
-            return this.colorService.addColor(action.color).pipe(
-              mergeMap((response) => {
-                const message = response.message;
-                const color = action.color;
-                this.toastrService.success(message);
-                const addColorSuccessAction = addColorSuccess({ message, color });
-                return of(addColorSuccessAction, loadColors()); // loadColors eylemini çağırın
-              })
-            );
+  addColor$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(addColor),
+      mergeMap((action) => {
+        return this.colorService.addColor(action.color).pipe(
+          mergeMap((response) => {
+            const message = response.message;
+            const color = action.color;
+            this.toastrService.success(message);
+            const addColorSuccessAction = addColorSuccess({ message, color });
+            return of(addColorSuccessAction, loadColors()); // loadColors eylemini çağırın
           })
         );
-      });
+      })
+    );
+  });
 
 
-      deleteColor$ = createEffect(() => {
-        return this.actions$.pipe(ofType(deleteColor), mergeMap((action) => {
-            return this.colorService.deleteColor(action.color).pipe(map((data) => {
-                const message = data.message;
-                const id = action.color.colorId
-                return deleteColorSuccess({ id:id, message })
-            }))
-        }))
-    },)
+  deleteColor$ = createEffect(() => {
+    return this.actions$.pipe(ofType(deleteColor), mergeMap((action) => {
+      return this.colorService.deleteColor(action.color).pipe(map((data) => {
+        const message = data.message;
+        const id = action.color.colorId
+        return deleteColorSuccess({ id: id, message })
+      }))
+    }))
+  },)
+
+  updateColor$ = createEffect(() => {
+    return this.actions$.pipe(ofType(updateColor), mergeMap((action) => {
+      return this.colorService.updateColor(action.color).pipe(mergeMap((data) => {
+        const updateColorSuccessAction = updateColorSuccess({color:action.color});
+        return of(updateColorSuccessAction, loadColors());
+      }))
+    }))
+  })
+
+
 
 
 
