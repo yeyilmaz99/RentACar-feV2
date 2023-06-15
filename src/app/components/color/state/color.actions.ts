ea3cf0c8c12f19b1d3356c325584e883bb77416a
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-15 05:06:17 +0300

    addBrand&addColor bug fix

--- src/app/components/color/state/color.actions.ts
+++ src/app/components/color/state/color.actions.ts
@@ -16,7 +16,7 @@ export const loadColors = createAction(LOAD_COLORS);
 export const loadColorsSuccess = createAction(LOAD_COLORS_SUCCESS, props<{colors: Color[]}>())
 
 export const addColor = createAction(ADD_COLOR_ACTION, props<{color:Color}>());
-export const addColorSuccess = createAction(ADD_COLOR_SUCCESS, props<{message:string}>())
+export const addColorSuccess = createAction(ADD_COLOR_SUCCESS, props<{color:Color, message:string}>())
 
 export const deleteColor = createAction(DELETE_COLOR_ACTION, props<{color:Color}>());
 export const deleteColorSuccess = createAction(DELETE_COLOR_SUCCESS, props<{id:number, message:string}>())
