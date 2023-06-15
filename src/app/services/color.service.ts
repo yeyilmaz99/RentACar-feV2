f5f1e8761a60f960566d5907833eef0715ac1937
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-15 03:38:54 +0300

    color-add

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Color } from '../models/color.model';
 import { HttpClient } from '@angular/common/http';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,20 @@ export class ColorService {
     let newPath = this.apiUrl + 'getall';
     return this.httpClient.get<ListResponseModel<Color>>(newPath);
   }
+
+  addColor(color:Color):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"add",color)
+  }
+
+  updateColor(color:Color):Observable<ResponseModel>{
+    return this.httpClient.patch<ResponseModel>(this.apiUrl+"update",color);
+  }
+
+  deleteColor(color:Color):Observable<ResponseModel>{
+    return this.httpClient.delete<ResponseModel>(this.apiUrl+"delete", {body:color});
+  }
+
+
 
 
 }
