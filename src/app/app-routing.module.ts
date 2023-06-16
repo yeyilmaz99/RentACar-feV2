901c59f4b15fddeda36898d9a3e2d35fd809412c
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-16 05:10:36 +0300

    admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CarDetailsComponent } from './components/car/car-details/car-details.component';
 import { ColorAddComponent } from './components/color/color-add/color-add.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 const routes: Routes = [
   {
@@ -13,8 +14,8 @@ const routes: Routes = [
   },
   { path: 'cars', loadChildren: () => import('./components/car/car.module').then(m => m.CarModule) },
   { path: 'brands', loadChildren:() => import('./components/brand/brand.module').then(m => m.BrandModule)},
-  { path: 'colors', loadChildren:() => import('./components/color/color.module').then(m => m.ColorModule)}
-
+  { path: 'colors', loadChildren:() => import('./components/color/color.module').then(m => m.ColorModule)},
+  { path: 'admin', component:AdminComponent, loadChildren:() => import ('./components/admin/admin.module').then(m => m.AdminModule)}
 ];
 
 @NgModule({
