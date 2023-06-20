e25678376c15f2cc2e256768c92bd160ceb20d3f
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-20 19:21:16 +0300

    adminPanel

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -8,6 +8,17 @@ import { AdminNavbarComponent } from './components/admin-navbar/admin-navbar.com
 import { AdminSidebarComponent } from './components/admin-sidebar/admin-sidebar.component';
 import { AdminComponent } from './admin.component';
 import { AdminCarListComponent } from './admin-car-list/admin-car-list.component';
+import { StoreModule } from "@ngrx/store";
+import { CAR_STATE_NAME } from "../car/state/car.selector";
+import { carReducer } from "../car/state/car.reducers";
+import { AdminUserListComponent } from './admin-user-list/admin-user-list.component';
+import { AdminRentalListComponent } from './admin-rental-list/admin-rental-list.component';
+import { AdminColorListComponent } from './admin-color-list/admin-color-list.component';
+import { ADMIN_PANEL_STATE_NAME } from "./store/admin.selector";
+import { adminReducer } from "./store/admin.reducer";
+import { EffectsModule } from "@ngrx/effects";
+import { CarEffects } from "../car/state/car.effects";
+import { AdminEffects } from "./store/admin.effects";
 
 
 
@@ -18,7 +29,8 @@ const routes: Routes = [
         path: '', children: [
             {path:'',pathMatch:"full",redirectTo:'dashboard'},
             {path: 'dashboard', component:AdminDashboardComponent},
-            {path: 'car-list', component:AdminCarListComponent}
+            {path: 'car-list', component:AdminCarListComponent},
+            {path: 'user-list', component:AdminUserListComponent}
         ]
     }
 
@@ -32,7 +44,10 @@ const routes: Routes = [
         AdminNavbarComponent,
         AdminSidebarComponent,
         AdminComponent,
-        AdminCarListComponent
+        AdminCarListComponent,
+        AdminUserListComponent,
+        AdminRentalListComponent,
+        AdminColorListComponent
     ],
 
     imports: [
@@ -42,6 +57,9 @@ const routes: Routes = [
         MatPaginatorModule,
         RouterModule.forChild(routes),
         FormsModule,
+        EffectsModule.forFeature([CarEffects,AdminEffects]),
+        StoreModule.forFeature(CAR_STATE_NAME,carReducer),
+        StoreModule.forFeature(ADMIN_PANEL_STATE_NAME,adminReducer)
     ],
     providers:[]
 })
