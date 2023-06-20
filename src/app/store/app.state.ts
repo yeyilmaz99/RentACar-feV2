0f9b59ee5878ac6e4af1e564a0e8f71ec8237baf
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-20 19:21:58 +0300

    adminPanel

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -12,9 +12,7 @@ import { colorReducer } from "../components/color/state/color.reducer";
 import { BRAND_STATE_NAME } from "../components/brand/state/brand.selector";
 import { BrandState } from "../components/brand/state/brand.state";
 import { brandReducer } from "../components/brand/state/brand.reducer";
-import { CAR_STATE_NAME } from "../components/car/state/car.selector";
-import { CarState } from "../components/car/state/car.state";
-import { carReducer } from "../components/car/state/car.reducers";
+
 
 
 export interface AppState {
@@ -22,7 +20,6 @@ export interface AppState {
     [SHARED_STATE_NAME]:SharedState;
     [COLOR_STATE_NAME]:ColorState;
     [BRAND_STATE_NAME]:BrandState;
-    [CAR_STATE_NAME]:CarState;
     router:RouterReducerState;
 }
 
@@ -31,6 +28,5 @@ export const appReducer = {
     [AUTH_STATE_NAME]:AuthReducer,
     [COLOR_STATE_NAME]:colorReducer,
     [BRAND_STATE_NAME]:brandReducer,
-    [CAR_STATE_NAME]:carReducer,
     router:routerReducer
 }
