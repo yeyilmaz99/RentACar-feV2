1571e007ed8ef976905cc3525f07669b96f0b808
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-20 19:21:22 +0300

    adminPanel

---
+++ src/app/components/admin/admin-color-list/admin-color-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-color-list',
+  templateUrl: './admin-color-list.component.html',
+  styleUrls: ['./admin-color-list.component.css']
+})
+export class AdminColorListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
