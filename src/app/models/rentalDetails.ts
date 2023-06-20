738e16043234f04729b553f881b23be61f28ff82
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-20 19:21:53 +0300

    adminPanel

---
+++ src/app/models/rentalDetails.ts
@@ -0,0 +1,10 @@
+export interface RentalDetails{
+    id:number;
+    carId:number;
+    brandName:string;
+    firstName:string;
+    lastName:string;
+    rentDate:Date;
+    returnDate:Date;
+    dailyPrice:number;
+}
