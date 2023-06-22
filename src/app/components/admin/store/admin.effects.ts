9f774b480234d9b56069d0064b6bf34e51120a36
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-22 21:35:11 +0300

    carImage Bugfix

--- src/app/components/admin/store/admin.effects.ts
+++ src/app/components/admin/store/admin.effects.ts
@@ -4,13 +4,14 @@ import { Actions, createEffect, ofType } from "@ngrx/effects"
 import { Store } from "@ngrx/store"
 import { ToastrService } from "ngx-toastr"
 import { AppState } from "src/app/store/app.state"
-import { loadActiveUsers, loadActiveUsersSuccess, loadUsers, loadUsersSuccess } from "./admin.actions"
-import { map, mergeMap } from "rxjs"
+import { deleteUser, loadActiveUsers, loadActiveUsersSuccess, loadRentals, loadRentalsSuccess, loadUsers, loadUsersSuccess, updateActiveUser, updateUser, updateUserSuccess } from "./admin.actions"
+import { map, mergeMap, of } from "rxjs"
 import { UserService } from "src/app/services/user-service/user-service.service"
+import { RentalService } from "src/app/services/rentalService/rental.service"
 
 @Injectable()
 export class AdminEffects {
-  constructor(private actions$: Actions, private store: Store<AppState>, private router: Router, private toastrService: ToastrService, private userService:UserService) { }
+  constructor(private actions$: Actions, private store: Store<AppState>, private router: Router, private toastrService: ToastrService, private userService:UserService, private rentalService:RentalService) { }
 
 
     loadUsers$ = createEffect (() => {
@@ -31,6 +32,36 @@ export class AdminEffects {
             }))
         }))
     })
+
+    updateUser$= createEffect(() => {
+        return this.actions$.pipe(ofType(updateUser), mergeMap((action) => {
+            return this.userService.updateUser(action.user).pipe(mergeMap((response) => {
+                const updateUserSuccessAction = updateUserSuccess({user:action.user});
+                return of (updateUserSuccessAction, loadUsers());
+            }))
+        }))
+    })
+
+
+    updateActiveUser$= createEffect(() => {
+        return this.actions$.pipe(ofType(updateActiveUser), mergeMap((action) => {
+            return this.userService.updateUser(action.activeUser).pipe(mergeMap((response) => {
+                const updateUserSuccessAction = updateUserSuccess({user:action.activeUser});
+                return of (updateUserSuccessAction, loadActiveUsers());
+            }))
+        }))
+    })
+
+    loadRentals$ = createEffect(() => {
+        return this.actions$.pipe(ofType(loadRentals), mergeMap((action) => {
+            return this.rentalService.getRentals().pipe(map((response => {
+                const rentals = response.data
+                return loadRentalsSuccess({rentals})
+            })))
+        }))
+    })
+
+
 
 
 
