fd09b743cd37430d9ffa3d60868dca7edd255493
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-22 21:35:10 +0300

    carImage Bugfix

--- src/app/components/admin/store/admin.actions.ts
+++ src/app/components/admin/store/admin.actions.ts
@@ -11,12 +11,16 @@ export const LOAD_USERS_SUCCESS = '[admin page] load users success';
 export const LOAD_ACTIVE_USERS = '[admin page] load active users';
 export const LOAD_ACTIVE_USERS_SUCCESS = '[admin page] load active users success';
 
-
-
 export const LOAD_RENTALS = '[admin page] load rentals';
 export const LOAD_RENTALS_SUCCESS = '[admin page] load rentals success';
 
+export const DELETE_USER_ACTION = '[admin page] delete user';
+export const DELETE_USER_SUCCESS = '[admin page] delete user success';
 
+export const UPDATE_USER_ACTION = '[admin page] update user';
+export const UPDATE_USER_SUCCESS = '[admin page] update user success';
+export const UPDATE_ACTIVE_USER_ACTION = '[admin page] update user';
+export const UPDATE_ACTIVE_USER_SUCCESS = '[admin page] update user success';
 
 export const loadUsers = createAction(LOAD_USERS);
 export const loadUsersSuccess = createAction(LOAD_USERS_SUCCESS, props<{users:UserList[]}>());
@@ -24,6 +28,14 @@ export const loadUsersSuccess = createAction(LOAD_USERS_SUCCESS, props<{users:Us
 export const loadActiveUsers = createAction(LOAD_ACTIVE_USERS);
 export const loadActiveUsersSuccess = createAction(LOAD_ACTIVE_USERS_SUCCESS, props<{activeUsers:ActiveUser[]}>());
 
-
 export const loadRentals = createAction(LOAD_RENTALS);
 export const loadRentalsSuccess = createAction(LOAD_RENTALS_SUCCESS, props<{rentals:RentalDetails[]}>());
+
+export const deleteUser = createAction(DELETE_USER_ACTION, props<{user:UserList | ActiveUser}>());
+export const deleteUserSuccess = createAction(DELETE_USER_ACTION, props<{userId: number}>());
+
+export const updateUser = createAction(UPDATE_USER_ACTION, props<{user:UserList}>());
+export const updateUserSuccess = createAction(UPDATE_USER_SUCCESS, props<{user: UserList }>());
+
+export const updateActiveUser = createAction(UPDATE_ACTIVE_USER_ACTION, props<{activeUser:ActiveUser}>());
+export const updateActiveUserSuccess = createAction(UPDATE_ACTIVE_USER_SUCCESS, props<{activeUser: ActiveUser }>());
