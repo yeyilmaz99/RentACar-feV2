542648bfecc8db4befdce4cdd86ec3f35df19195
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-22 21:35:12 +0300

    carImage Bugfix

--- src/app/components/admin/store/admin.reducer.ts
+++ src/app/components/admin/store/admin.reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from "@ngrx/store"
 import { initialState } from "./admin.state"
-import { loadActiveUsersSuccess, loadUsersSuccess } from "./admin.actions";
+import { loadActiveUsersSuccess, loadRentals, loadRentalsSuccess, loadUsersSuccess, updateActiveUserSuccess, updateUserSuccess } from "./admin.actions";
 
 
 const _adminReducer = createReducer(initialState, 
@@ -13,6 +13,27 @@ const _adminReducer = createReducer(initialState,
         return {
             ...state,
             activeUsers:action.activeUsers
+        }
+    }),on (updateUserSuccess, (state,action) => {
+        const updatedUsers = state.users.map(user => {
+            return action.user.id === user.id ? action.user : user;
+        })
+        return {
+            ...state,
+            users:updatedUsers
+        }
+    }), on(updateActiveUserSuccess, (state,action) => {
+        const updatedActiveUsers = state.activeUsers.map(activeUser => {
+            return action.activeUser.id === activeUser.id ? action.activeUser : activeUser;
+        })
+        return {
+            ...state,
+            activeUsers:updatedActiveUsers
+        }
+    }), on(loadRentalsSuccess, (state,action) => {
+        return {
+            ...state,
+            rentals: action.rentals
         }
     })
     )
