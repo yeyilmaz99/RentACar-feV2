f5abd1a9f8fb20cf9809cefeab416d352011af51
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-22 21:35:21 +0300

    carImage Bugfix

---
+++ src/app/models/carAndImageDto.ts
@@ -0,0 +1,11 @@
+export interface CarAndImageDto{
+    id:number;
+    brandId:number;
+    colorId:number;
+    carName:string;
+    modelYear:number;
+    dailyPrice:number;
+    description:string;
+    findeksPoint:number;
+    image:File
+}
