d244d98fb7b4367a5e6fe2fb356c3fd3d205d52f
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-22 21:35:15 +0300

    carImage Bugfix

--- src/app/components/admin/store/admin.state.ts
+++ src/app/components/admin/store/admin.state.ts
@@ -1,4 +1,5 @@
 import { ActiveUser } from "src/app/models/activeUser";
+import { Rental } from "src/app/models/rental";
 import { UserList } from "src/app/models/userList";
 
 
@@ -6,9 +7,11 @@ import { UserList } from "src/app/models/userList";
 export interface AdminState {
     users:UserList[];
     activeUsers:ActiveUser[];
+    rentals:Rental[];
 }
 
 export const initialState:AdminState = {
     users:null,
-    activeUsers:null
+    activeUsers:null,
+    rentals:null
 }
