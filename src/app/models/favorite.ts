fd8a3bc54a486bdb08187337e084b92b618d373e
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-23 18:56:40 +0300

    favorites

---
+++ src/app/models/favorite.ts
@@ -0,0 +1,10 @@
+export interface Favorite {
+    userId:number;
+    carId:number;
+    userName:string;
+    carName:string;
+    brandName:string;
+    colorName:string;
+    dailyPrice:number;
+    description:string;
+}
