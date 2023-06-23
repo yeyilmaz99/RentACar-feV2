bb896fce06beb822be8679ead765cd6c575187da
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-23 19:26:53 +0300

    favorites

--- src/app/components/car/state/car.state.ts
+++ src/app/components/car/state/car.state.ts
@@ -1,5 +1,6 @@
 import { Car } from "src/app/models/car.model";
 import { CarImage } from "src/app/models/carImage";
+import { Favorite } from "src/app/models/favorite";
 
 
 
@@ -7,10 +8,12 @@ export interface CarState {
     cars:Car[];
     selectedCar:Car
     carImages:CarImage[];
+    favorites:Favorite[];
 }
 
 export const initialState:CarState = {
     cars:null,
     selectedCar:null,
-    carImages:null
+    carImages:null,
+    favorites:null,
 }
