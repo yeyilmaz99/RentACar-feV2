76541283af66949c266d26906ac5a3e94eeaaaac
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-23 19:26:55 +0300

    favorites

--- src/app/components/favorite/favorite.component.ts
+++ src/app/components/favorite/favorite.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
 import { Favorite } from 'src/app/models/favorite';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { FavoriteService } from 'src/app/services/favorite/favorite.service';
+import { AppState } from 'src/app/store/app.state';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
+import { getUserId, isAuthenticated } from '../auth/state/auth.selector';
+import { loadFavoriteCars } from '../car/state/car.actions';
+import { getFavorites } from '../car/state/car.selector';
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
@@ -11,19 +17,42 @@ import Swal from 'sweetalert2/dist/sweetalert2.js';
 })
 export class FavoriteComponent implements OnInit {
   // claims:Claims | undefined = {email:"",fullName:"",roles:[""],userId:0};
-  dataLoaded:boolean = false;
   favorites:Favorite[] = []
-
+  isAuthenticated:Observable<boolean>;
+  userId:number;
   constructor(
     private favoriteService:FavoriteService,
     private toastrService:ToastrService,
-    private authService:AuthService
+    private authService:AuthService,
+    private store:Store<AppState>
     ) { }
 
 
   ngOnInit(): void {
-    
+    this.isAuthenticated = this.store.select(isAuthenticated);
+     this.store.select(getUserId).subscribe(response => {
+      this.userId = response
+    });
+    this.getFavorites();
+
+
   }
+
+  getFavorites(){
+    if(this.isAuthenticated){
+      const id = this.userId
+      this.store.dispatch(loadFavoriteCars({id}))
+      this.store.select(getFavorites).subscribe(response => {
+        this.favorites = response
+      })
+    }
+  }
+
+
+
+
+
+
   // ngOnInit(): void {
   //   // this.getClaims();
   //   this.getFavoritesByUserId();
