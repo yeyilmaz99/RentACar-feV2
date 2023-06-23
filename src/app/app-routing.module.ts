49c177e2785bc97939548ae1fe2576d8f7ffa845
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-23 18:56:30 +0300

    favorites

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { CarDetailsComponent } from './components/car/car-details/car-details.component';
 import { ColorAddComponent } from './components/color/color-add/color-add.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { FavoriteComponent } from './components/favorite/favorite.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
   {
     path: 'auth', loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
   },
+  {path: "favorites", component:FavoriteComponent},
   { path: 'cars', loadChildren: () => import('./components/car/car.module').then(m => m.CarModule) },
   { path: 'brands', loadChildren:() => import('./components/brand/brand.module').then(m => m.BrandModule)},
   { path: 'colors', loadChildren:() => import('./components/color/color.module').then(m => m.ColorModule)},
