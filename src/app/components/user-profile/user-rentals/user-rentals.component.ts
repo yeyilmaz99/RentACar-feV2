d84301a41dab6c377cd21dc634f9cac0d49d0f36
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-28 00:34:48 +0300

    user-profile

---
+++ src/app/components/user-profile/user-rentals/user-rentals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-rentals',
+  templateUrl: './user-rentals.component.html',
+  styleUrls: ['./user-rentals.component.css']
+})
+export class UserRentalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
