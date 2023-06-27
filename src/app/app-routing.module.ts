da878a6dd5e37b25917de4e681a81b2826ffeefc
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-28 00:33:51 +0300

    user-profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { CarDetailsComponent } from './components/car/car-details/car-details.co
 import { ColorAddComponent } from './components/color/color-add/color-add.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { FavoriteComponent } from './components/favorite/favorite.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 const routes: Routes = [
   {
@@ -17,7 +18,8 @@ const routes: Routes = [
   { path: 'cars', loadChildren: () => import('./components/car/car.module').then(m => m.CarModule) },
   { path: 'brands', loadChildren:() => import('./components/brand/brand.module').then(m => m.BrandModule)},
   { path: 'colors', loadChildren:() => import('./components/color/color.module').then(m => m.ColorModule)},
-  { path: 'admin', component:AdminComponent, loadChildren:() => import ('./components/admin/admin.module').then(m => m.AdminModule)}
+  { path: 'admin', component:AdminComponent, loadChildren:() => import ('./components/admin/admin.module').then(m => m.AdminModule)},
+  {path: 'user',loadChildren:() => import('./components/user-profile/user.module').then (m=> m.UserModule)}
 ];
 
 @NgModule({
