09f478cf93853aef57d85f73930a109623bf20e5
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-28 00:34:33 +0300

    user-profile

---
+++ src/app/components/user-profile/user.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import { UserFavoritesComponent } from './user-favorites/user-favorites.component';
+import { UserRentalsComponent } from './user-rentals/user-rentals.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+
+
+
+export const UserRoutes:Routes = [
+    {path: '' ,pathMatch:'full' ,redirectTo:'info'},
+    {path: 'info', component:UserInfoComponent},
+    {path: 'settings', component:UserSettingsComponent}
+]
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(UserRoutes),
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatTabsModule
+
+  ],
+  declarations: [
+    UserRentalsComponent,
+    UserSettingsComponent,
+    UserInfoComponent,
+    UserFavoritesComponent
+  ]
+})
+
+export class UserModule {}
