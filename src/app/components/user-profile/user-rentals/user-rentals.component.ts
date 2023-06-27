085b98aaa786276c45f13f2c188b56da7e5cf383
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-28 01:15:14 +0300

    user-proife

--- src/app/components/user-profile/user-rentals/user-rentals.component.ts
+++ src/app/components/user-profile/user-rentals/user-rentals.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { loadUserRentals } from '../../car/state/car.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.state';
+import { getUserId } from '../../auth/state/auth.selector';
+import { getUserRentals } from '../../car/state/car.selector';
+import { Rental } from 'src/app/models/rental';
 
 @Component({
   selector: 'app-user-rentals',
@@ -6,10 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-rentals.component.css']
 })
 export class UserRentalsComponent implements OnInit {
-
-  constructor() { }
+  userId:number;
+  rentals:Rental[];
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.store.select(getUserId).subscribe(response => {
+      this.userId = response
+     })
+     this.getRentals()
   }
 
+
+  getRentals(){
+    const userId = this.userId
+    this.store.dispatch(loadUserRentals({userId}))
+    this.store.select(getUserRentals).subscribe(response =>{
+      this.rentals = response;
+    })
+  }
 }
