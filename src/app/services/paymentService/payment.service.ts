95c0560a3b49d35fd41c19b0819db85850d8b554
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-06-28 04:30:34 +0300

    rental

---
+++ src/app/services/paymentService/payment.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Payment } from 'src/app/models/payment';
+import { ResponseModel } from 'src/app/models/responseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+  apiUrl = "https://localhost:5001/api/Payment/add"
+
+  constructor(
+    private httpClient:HttpClient
+  ) { }
+
+  add(payment:Payment):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl,payment);
+  }
+
+}
