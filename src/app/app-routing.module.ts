f8a7e6404b0f5a4824af71d34bd8a485258cd848
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-07-19 16:17:01 +0300

    contactUs-aboutUs & Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { AdminComponent } from './components/admin/admin.component';
 import { FavoriteComponent } from './components/favorite/favorite.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { UserRentalsComponent } from './components/user-profile/user-rentals/user-rentals.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { AdminGuard } from './guards/admin.guard';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {
@@ -15,12 +19,14 @@ const routes: Routes = [
   {
     path: 'auth', loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
   },
-  {path: "favorites", component:FavoriteComponent},
+  {path: "favorites", component:FavoriteComponent, canActivate:[LoginGuard]},
   { path: 'cars', loadChildren: () => import('./components/car/car.module').then(m => m.CarModule) },
   { path: 'brands', loadChildren:() => import('./components/brand/brand.module').then(m => m.BrandModule)},
   { path: 'colors', loadChildren:() => import('./components/color/color.module').then(m => m.ColorModule)},
-  { path: 'admin', component:AdminComponent, loadChildren:() => import ('./components/admin/admin.module').then(m => m.AdminModule)},
-  {path: 'user',loadChildren:() => import('./components/user-profile/user.module').then (m=> m.UserModule)},
+  { path: 'admin', component:AdminComponent, loadChildren:() => import ('./components/admin/admin.module').then(m => m.AdminModule),canActivate:[AdminGuard]},
+  { path: 'user',loadChildren:() => import('./components/user-profile/user.module').then (m=> m.UserModule),canActivate:[LoginGuard]},
+  { path: 'about', component:AboutUsComponent},
+  { path: 'contact', component:ContactUsComponent}
 ];
 
 @NgModule({
