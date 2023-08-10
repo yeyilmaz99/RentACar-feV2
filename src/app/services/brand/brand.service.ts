ab15a199ad63e76be9533c960c2df8f0a69a8f88
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-08-10 22:04:12 +0300

    BrandImages

--- src/app/services/brand/brand.service.ts
+++ src/app/services/brand/brand.service.ts
@@ -2,32 +2,39 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Brand } from 'src/app/models/brand.model';
+import { BrandImage } from 'src/app/models/brandImage';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
 import { ResponseModel } from 'src/app/models/responseModel';
+import { SingleResponseModel } from 'src/app/models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrandService {
-  apiUrl = "https://apiv2.rentacar.yeyilmaz.online/api/Brands/";
-  // apiUrl = "https://localhost:5001/api/Brands/";
+  // apiUrl = "https://apiv2.rentacar.yeyilmaz.online/api/Brands/";
+  apiUrl = "https://localhost:5001/api/";
   constructor(private httpClient:HttpClient) { }
 
   getBrands(): Observable<ListResponseModel<Brand>> {
-    let newPath = this.apiUrl + 'getall';
+    let newPath = this.apiUrl + 'Brands/getdetail';
     return this.httpClient.get<ListResponseModel<Brand>>(newPath);
   }
 
-  addBrand(brand:Brand):Observable<ResponseModel>{
-    return this.httpClient.post<ResponseModel>(this.apiUrl + "add", brand);
+  addBrand(formData:FormData):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl + "Brands/add", formData);
   }
 
   deleteBrand(brand:Brand):Observable<ResponseModel>{
-    return this.httpClient.delete<ResponseModel>(this.apiUrl+"delete", {body:brand});
+    return this.httpClient.delete<ResponseModel>(this.apiUrl+"Brands/delete", {body:brand});
   }
 
   updateBrand(brand:Brand):Observable<ResponseModel>{
-    return this.httpClient.patch<ResponseModel>(this.apiUrl+"update",brand);
+    return this.httpClient.patch<ResponseModel>(this.apiUrl+"Brands/update",brand);
   }
+
+  getBrandImage(brandId:number):Observable<SingleResponseModel<BrandImage>>{
+    return this.httpClient.get<SingleResponseModel<BrandImage>>(this.apiUrl+"BrandImages/get?brandId="+brandId);
+  }
+
 
 }
