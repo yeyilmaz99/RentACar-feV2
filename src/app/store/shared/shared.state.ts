cb8a46f0f5be2b266c6745754e4e66bda3fc7bf5
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-08-14 04:10:23 +0300

    loading spinner update

--- src/app/store/shared/shared.state.ts
+++ src/app/store/shared/shared.state.ts
@@ -2,10 +2,12 @@
 
 export interface SharedState {
     showLoading: boolean;
+    loadingFrom:string;
     errorMessage:string;
 }
 
 export const initialState: SharedState = {
+    loadingFrom:'',
     showLoading: false,
     errorMessage: ''
 }
