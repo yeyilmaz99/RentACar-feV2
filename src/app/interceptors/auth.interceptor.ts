5656d4810f3d1b1aeaa13ceea7cfb5ec749d4aec
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-11-29 12:47:35 +0300

    iyzicoUpdate

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -5,27 +5,20 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable, exhaustMap, take } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { AppState } from '../store/app.state';
-import { getToken } from '../components/auth/state/auth.selector';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class AuthTokenInterceptor implements HttpInterceptor {
-    constructor(private store:Store<AppState>){}
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+export class AuthInterceptor implements HttpInterceptor {
 
-        return this.store.select(getToken).pipe(take(1),exhaustMap(token => {
-            if (!token) {
-                return next.handle(req);
-            }
-            let newRequest :HttpRequest<any>;
-            newRequest = req.clone({headers: req.headers.set("Authorization", "Bearer " + token)})
-            return next.handle(newRequest);
-        }))
+  constructor() {}
 
-    }
-
-
-
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let token = localStorage.getItem("token");
+    let newRequest : HttpRequest<any>;
+    newRequest = request.clone({
+      headers: request.headers.set("Authorization", "Bearer " + token)
+    })
+    console.log(newRequest)
+    return next.handle(newRequest);
+  }
 }
