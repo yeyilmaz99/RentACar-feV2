543be5fa5e4f029d1877b773075cea4e3bb0481d
yeyilmaz99 <yeyilmaz34@gmail.com>
2024-02-02 19:35:18 +0300

    update

--- src/app/services/paymentService/payment.service.ts
+++ src/app/services/paymentService/payment.service.ts
@@ -9,8 +9,8 @@ import { ResponseModel } from 'src/app/models/responseModel';
   providedIn: 'root'
 })
 export class PaymentService {
-  // apiUrl = "https://apiv2.rentacar.yeyilmaz.online/api/Payment/"
-  apiUrl = "https://localhost:5001/api/Payment/"
+  apiUrl = "https://apiv2.rentacar.yeyilmaz.online/api/Payment/"
+  // apiUrl = "https://localhost:5001/api/Payment/"
 
   constructor(
     private httpClient:HttpClient
